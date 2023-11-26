edd0cf6841a1fc42f8f05103a05e5aba09d2ad8f
magicana-j <zima0524@hotmail.co.jp>
2023-11-26 10:27:11 +0900

    Created files

---
+++ install-latest-docker.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
---
+++ remove-old-docker.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt remove $pkg; done
